b1f6fbdd7ac66677e05cde1c8435d7d468b6d1ab
ide-ryunosuke <r.i1996423@gmail.com>
2019-04-15 20:01:56 +0900

    Add-new-member-page

--- app/controllers/members_controller.rb
+++ app/controllers/members_controller.rb
@@ -1,4 +1,15 @@
 class MembersController < ApplicationController
+
+  before_action :move_to_index, only: :new
+
   def index
+  end
+
+  def new
+  end
+
+  private
+  def move_to_index
+    redirect_to root_path unless user_signed_in?
   end
 end
