847dec5d0d90e170559b012ec62ce7e7e7ee39f5
ide-ryunosuke <r.i1996423@gmail.com>
2019-04-17 14:33:50 +0900

    Add-routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,12 @@
 Rails.application.routes.draw do
   root 'members#index'
-  
+
   devise_for :users, controllers: {
     registrations: 'users/registrations',
     sessions: 'users/sessions'
   }
 
   resources :members do
+    resources :scores, only: [:new, :create]
   end
 end
