3a0cbcc58d05cedf8b35db8ab9b5b0b20f28c571
ide-ryunosuke <r.i1996423@gmail.com>
2019-04-18 18:50:27 +0900

    display-score

--- app/controllers/members_controller.rb
+++ app/controllers/members_controller.rb
@@ -4,7 +4,35 @@ class MembersController < ApplicationController
 
   def index
     @member = Member.where(user_id: current_user&.id)
+    score_user = Score.where(user_id: current_user&.id)
+    @plate_appearance = score_user.sum(:plate_appearance)
+    @at_bat = score_user.sum(:at_bat)
+    @hit = score_user.sum(:hit)
+    @runs_batted_in = score_user.sum(:runs_batted_in)
+    @sacrafice = score_user.sum(:sacrafice)
+    @walk = score_user.sum(:walk)
+    @strikeout = score_user.sum(:strikeout)
+    @stolen_base = score_user.sum(:stolen_base)
+    @run = score_user.sum(:run)
+    @home_run = score_user.sum(:home_run)
+    @game_count = score_user.length
+    batting_average = @hit.to_f / @at_bat
+    @batting_average_round = batting_average.round(3)
+    # on_base = (@hit + @walk).to_f / (@at_bat + @walk + )
+    # @on_base_percentage
   end
+  # 出塁率＝（安打数+四球数+死球数）÷（打数+四球数+死球数+犠飛数）
+  #
+  # |plate_appearance|integer|default: 0|     <!-- 打席数 -->
+  # |at_bat|integer|default: 0|               <!-- 打数 -->
+  # |hit|integer|default: 0|                  <!-- 安打 -->
+  # |runs_batted_in|integer|default: 0|       <!-- 打点 -->
+  # |sacrafice|integer|default: 0|            <!-- 犠打 -->
+  # |walk|integer|default: 0|                 <!-- 四死球 -->
+  # |strikeout|integer|default: 0|            <!-- 三振 -->
+  # |stolen_base|integer|default: 0|          <!-- 盗塁 -->
+  # |run|integer|default: 0|                  <!-- 得点 -->
+  # |home_run|integer|default: 0|             <!-- 本塁打 -->
 
   def new
     @member = Member.new
